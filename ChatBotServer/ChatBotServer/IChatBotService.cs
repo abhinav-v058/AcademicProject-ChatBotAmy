b2027d99e15f212745f00f2ab01fb70453e116de
abhinav-v058 <abhinavv@gwu.edu>
2016-11-16 12:11:43 -0500

    LoadedExistingCode

---
+++ ChatBotServer/ChatBotServer/IChatBotService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatBotServer
+{
+    [ServiceContract]
+    public interface IChatBotService
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        ChatData GetDataUsingDataContract(ChatData composite);
+                
+    }
+        
+    [DataContract]
+    public class ChatData
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
